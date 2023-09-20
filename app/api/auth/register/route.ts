f953d95016073004e9efd74bb6303a901198ce02
Thomas Aunvik <contact@thaun.dev>
2023-09-20 21:19:41 +0200

    Auth

---
+++ app/api/auth/register/route.ts
@@ -0,0 +1,40 @@
+import prisma from "@/lib/prisma";
+import { NextResponse } from "next/server";
+import { hash } from "bcrypt";
+
+export async function POST(req: Request) {
+  const { username, email, password } = await req.json();
+
+  const emailExist = await prisma.user.findUnique({
+    where: {
+      email: email,
+    },
+  });
+
+  const usernameExist = await prisma.user.findUnique({
+    where: {
+      username: username,
+    },
+  });
+
+  if (emailExist) {
+    return NextResponse.json(
+      { error: "Email already exists" },
+      { status: 400 }
+    );
+  } else if (usernameExist) {
+    return NextResponse.json(
+      { error: "Username already exists" },
+      { status: 400 }
+    );
+  } else {
+    const user = await prisma.user.create({
+      data: {
+        username,
+        email,
+        password: await hash(password, 14),
+      },
+    });
+    return NextResponse.json({ ...user });
+  }
+}
