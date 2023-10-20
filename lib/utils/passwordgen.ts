ed023fa246b86ce853a8337066bae46f2ee43128
Thomas Aunvik <contact@thaun.dev>
2023-10-20 02:00:52 +0200

    Password Fix

--- lib/utils/passwordgen.ts
+++ lib/utils/passwordgen.ts
@@ -9,31 +9,37 @@ export interface Password {
   };
 }
 
+const str2ab = (str: string): ArrayBuffer => {
+  const buffer = new ArrayBuffer(str.length * 2);
+  const bufferInterface = new Uint8Array(buffer);
+  Array.from(str)
+    .forEach((_, index: number) => bufferInterface[index] = str.charCodeAt(index));
+  return buffer;
+}
+
 const getDerivation = async (
-  hash: string,
-  salt: Uint32Array,
+  salt: ArrayBuffer | ArrayBufferLike,
   password: string,
   iterations: number,
   keyLength: number,
   digest: string,
   algorithm: string,
 ) => {
-  const textEncoder = new TextEncoder();
-  const passwordBuffer = textEncoder.encode(password);
+  const passwordBuffer = str2ab(password);
 
   const pwHash = await crypto.subtle.digest("SHA-256", passwordBuffer);
 
   const importedKey = await crypto.subtle.importKey(
     "raw",
     pwHash,
-    digest,
+    "PBKDF2",
     false,
     ["deriveBits"],
   );
 
   const params = {
     name: algorithm,
-    hash: hash,
+    hash: digest,
     salt: salt,
     iterations: iterations,
   };
@@ -48,19 +54,18 @@ const getDerivation = async (
 };
 
 export const generateStoredPassword = async (password: string) => {
-  const salt = crypto.getRandomValues(new Uint32Array(16));
+  const salt = crypto.getRandomValues(new Uint8Array(16));
 
   const hashIteration = 27500;
   const digest = "SHA-256";
   const type = "PBKDF2";
-  const algorithm = type + "-" + digest;
+  const algorithm = type + ";" + digest;
 
   var derivation = await getDerivation(
-    process.env.NEXTAUTH_SECRET ?? "rrandom",
     salt,
     password,
     hashIteration,
-    8,
+    256,
     digest,
     type,
   );
@@ -82,13 +87,13 @@ export const generateStoredPassword = async (password: string) => {
 };
 
 export const verifyPassword = async (input: string, pass: Password) => {
-  const type = pass.credentialsData.algorithm.split("-")[0];
-  const digest = pass.credentialsData.algorithm.split("-")[1];
+  const type = pass.credentialsData.algorithm.split(";")[0];
+  const digest = pass.credentialsData.algorithm.split(";")[1];
 
   const saltArray = Array.from(atob(pass.secretData.salt)).map((char) =>
     char.charCodeAt(0),
   );
-  const salt = Uint32Array.from(saltArray);
+  const salt = Uint8Array.from(saltArray);
 
   const passArray = atob(pass.secretData.value);
 
@@ -97,7 +102,7 @@ export const verifyPassword = async (input: string, pass: Password) => {
     salt,
     passArray,
     pass.credentialsData.hashIteration,
-    64,
+    passArray.length,
     digest,
     type,
   );
@@ -105,7 +110,7 @@ export const verifyPassword = async (input: string, pass: Password) => {
 
 export const verifyCredential = async (
   input: string,
-  salt: Uint32Array,
+  salt: ArrayBuffer | ArrayBufferLike,
   hash: string,
   iterations: number,
   hashBytes: number,
@@ -113,11 +118,10 @@ export const verifyCredential = async (
   type: string,
 ) => {
   var derivation = await getDerivation(
-    process.env.NEXTAUTH_SECRET ?? "rrandom",
     salt,
     input,
     iterations,
-    8,
+    hashBytes,
     digest,
     type,
   );
